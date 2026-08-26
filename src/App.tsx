7266ffa6ad65243b479eaa6cb55cf78c7d2c34ca
azharf99 <azharfaturohman29@gmail.com>
2026-08-26 23:18:46 +0700

    chore: add a blank line for improved code readability in App component

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import ForgotPassword from './pages/Auth/ForgotPassword'
 import ResetPassword from './pages/Auth/ResetPassword'
 import { Toaster } from 'react-hot-toast'
 
+
 function App() {
   const { state } = useAuth()
   const { isAuthenticated, loading } = state
